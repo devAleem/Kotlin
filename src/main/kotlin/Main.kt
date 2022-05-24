77d95c984a0e32c5d33a3a90108fcc3e1b19e8b5
Aleem Awan <aleemawan366@outlook.com>
2022-05-24 16:05:03 +0500

    changes in primary constructor

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,3 @@
 fun main(args: Array<String>) {
-
+    val person:Person = Person("Aleem","Awan")
 }
--- src/main/kotlin/Person.kt
+++ src/main/kotlin/Person.kt
@@ -1,13 +1,18 @@
 class Person constructor(firstName:String,lastName:String) {
-    private lateinit var firstName: String
-    private lateinit var lastName: String
+    private lateinit var name:String
+
+    val firstProperty = "Name : $firstName  $lastName".also(::println)
 
     init {
-        this.firstName = firstName
-        this.lastName = lastName
+        this.name = "${firstName.uppercase()} ${lastName.uppercase()}"
+        println("First block that initializes Name $firstName $lastName")
     }
-    override fun toString(): String {
-        return "Person(firstName='$firstName', lastName='$lastName')"
+
+    val secondProperty = "Second Property : ${firstName.length}".also(::println)
+
+    init{
+        println("Second block that initializes length ${firstName.length}}")
     }
+
 
 }
